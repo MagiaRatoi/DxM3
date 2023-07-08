80580ddd1d656eb000e32c81c15605694dd0f781
MagiaRatoi <112208505+MagiaRatoi@users.noreply.github.com>
2023-07-08 23:19:21 +0300

    Update Networth.js

--- utils/Networth.js
+++ utils/Networth.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 const { getNetworth } = require("skyhelper-networth");
 const networthParser = require("./networthParser.js");
 const config = require('../config.json');
-const apiKey = config.networth.apiKey;
+const apiKey = "52621312-30c0-4701-ac8e-afe34b6ab8e5"
 
 async function networthCalc(uuid) {
     const apiUrl = "https://api.hypixel.net/skyblock/profiles";
